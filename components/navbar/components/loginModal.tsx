a26eb8b72bf5cf6a5e2dd2a362df39cb5d781c51
mohamad-serhan1 <mohd.ser19@gmail.com>
2023-11-22 09:17:36 +0200

    finalizing

--- components/navbar/components/loginModal.tsx
+++ components/navbar/components/loginModal.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import signUp from "./signupModal";
 
 export default function Login() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
