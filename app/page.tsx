2efde099a3e50b313139441ae2a5e855eaff1660
mohamad-serhan1 <mohd.ser19@gmail.com>
2023-12-04 23:46:13 +0200

    2test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 import Container from "@/components/Container";
-import ProductCard from "@/components/product/productCard";
-import { fetchProd } from "@/components/product/fetchProduct";
-
 export default async function Home() {
     return (
     <main className="flex-grow  flex-col items-center pt-10 text-gray-600">
