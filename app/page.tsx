96e75308555caf2588c450acd59cda87af888a10
mohamad-serhan1 <mohd.ser19@gmail.com>
2023-12-04 22:48:31 +0200

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,16 @@
 import Container from "@/components/Container";
 import ProductCard from "@/components/product/productCard";
+import axios from "axios";
 
- export async function fetchProd() {
-  const res = await fetch("http://localhost:3000/api/products", {
-    next: {
-      revalidate: 10,
-    },
-  });
-
-  
-  const data = await res.json();
- 
-  return data.products;
-}
 
 export default async function Home() {
+   async function fetchProd(): Promise<any[]> {
+    const res = await axios.get("http://localhost:3000/api/products");
+  
+  
+  
+    return res.data;
+  }
   const products = await fetchProd();
 
   return (
@@ -22,8 +18,7 @@ export default async function Home() {
       <Container>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-8">
           {products.map((product: any) => (
-          <ProductCard key={product.id} data={product} />
-           
+            <ProductCard key={product.id} data={product} />
           ))}
         </div>
       </Container>
