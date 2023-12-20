492327cf5d5512f2434170540572db1f58108c9b
mohamad-serhan1 <mohd.ser19@gmail.com>
2023-12-20 11:29:24 +0200

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,12 @@
 import Container from "@/components/Container";
 import ProductCard from "@/components/product/productCard";
-import axios from "axios";
-
-
-const fetchProd = async() => {
-  // get user id
-  try{
-
-    const products = await axios.get(
-      `http://localhost:3000/api/products`
-      );
-      return products.data.products;
-    }
-catch(error) {
-      console.log("Can't fetch products: " + error);
-    };
-};
+import Fetch from "@/components/product/fetchProduct";
 
 export default async function Home() {
-  const products = await fetchProd();
 
   return (
     <main className="flex-grow  flex-col items-center pt-10 text-gray-600">
-      <Container>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-8">
-          {products.map((product: any) => (
-          <ProductCard key={product.id} data={product} />
-           
-          ))}
-        </div>
-      </Container>
+    <Fetch/>
     </main>
   );
 }
--- components/product/fetchProduct.ts
+++
@@ -1,7 +0,0 @@
-
-import axios from 'axios';
-
-export async function fetchProd() {
-  const res = await axios.get('http://localhost:3000/api/products');
-  return res.data;
-}
